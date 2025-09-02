9ef00bfd3305ce8b28e3c11e1e6c4cd9b367199f
Rodrigo Peroba de Souza <88716274+RodPeroba@users.noreply.github.com>
2025-09-02 11:56:20 +0000

    Aula de apresentação do Django 02/09/2025

---
+++ MainFolder/site-alo-mundo/MeuSite/MeuApp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request,"MeuApp/home.html")
+
+def segundaPagina(request):
+    return render(request,"MeuApp/segundaPagina.html")
